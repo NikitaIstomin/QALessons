98f423316367a3e1e0b1a8fb2f3d346a3f29908b
Никита <istomin_nike@mail.ru>
2024-09-10 15:40:34 +0300

    Добавлено задание 12_testng.

--- src/Junit5.java
+++ src/Junit5.java
@@ -1,29 +1,29 @@
+import org.junit.jupiter.api.Assertions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
-public class Testng {
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Junit5 {
     //1!=1
     @Test
     public void factorialOneTest(){
-        Assert.assertEquals(Factorial.factorial(1), 1);
+        Assertions.assertEquals(Factorial.factorial(1), 1);
     }
     //5!=120
     @Test
     public void FactorialOrdinaryNumberTest(){
-        Assert.assertEquals(Factorial.factorial(5), 120);
+        Assertions.assertEquals(Factorial.factorial(5), 120);
     }
     //0!=1
     @Test
     public void FactorialZeroNumberTest(){
-        Assert.assertEquals(Factorial.factorial(5), 120);
+        Assertions.assertEquals(Factorial.factorial(5), 120);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class,
             expectedExceptionsMessageRegExp = "Значение не должно быть отрицательным")
-    public void factorialNegativeNumberTest() {
+    public void FactorialNegativeNumberTest() {
         Factorial.factorial(-1);
     }
-
 }
-
